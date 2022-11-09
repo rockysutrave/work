356cd2db6b8ab7140aad6c3ce60d15e0b6121c91
Rakesh <rockysutrave@gmail.com>
2022-11-08 22:59:10 -0800

    python files added

---
+++ main.py
@@ -0,0 +1,4 @@
+x = input("Enter the number1:")
+y = input("Enter the number1:")
+sum = float(x) + float(y)
+print(sum)
---
+++ work1.py
@@ -0,0 +1,4 @@
+x = input("Enter the number1:")
+y = input("Enter the number2:")
+product = float(x) * float(y)
+print("Product of 2 numbers is : ", product)
